0ab7a2c06c53eaf7df2f3964e0d3f7de0e084aaf
Angelina Ayala <ayalam1506@missioncisd.org>
2018-06-01 13:47:31 +0000

    final project depol

---
+++ script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function{
+//     (".dropdown-trigger").dropdown();
+    
+// })
+document.addEventListener('DOMContentLoaded', function() {
+   var elems = document.querySelectorAll('.dropdown-trigger');
+   var instances = M.Dropdown.init(elems);
+ });
